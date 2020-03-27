a9009769cd4fd6132fcc30aab331e0e6947bc53f
FitzingerJ <jacki.fitzinger@gmail.com>
2020-03-27 21:33:36 +0100

    datebnak so halb

---
+++ php/createUser.php
@@ -0,0 +1,36 @@
+<?php
+$_db_host = "localhost";
+$_db_database = "web";
+$_db_username = "web";
+$_db_password = "web";
+
+session_start();
+
+$conn = new mysqli($_db_host,$_db_username,$_db_password,$_db_database);
+if ($conn->connect_error){
+    die("Connection failed, ". $conn->connect_error);
+}
+
+if (!empty($_POST['submit'])) {
+    $_username = $conn->real_escape_string($_POST['username']);
+    $_password = $conn->real_escape_string($_POST['password']);
+
+    if (strcmp($_password, $conn->real_escape_string($_POST['password_2'])) != 0) {
+        include("createUser.html");
+        exit;
+    }
+
+    $_password = "saver". $_password;
+
+    $insertStatement = "INSERT INTO login_username (username, password, user_deleted, last_login) VALUES ('$_username', md5('$_password'), 0, NOW())";
+    if ($res = $conn->query($insertStatement)) {
+        include("login.html");
+    } else {
+        // No insertion. User could not be added. Maybe user $_username already exists.
+        include("createUser.html");
+    }
+} else {
+    include("createUser.html");
+}
+
+$conn->close();
---
+++ php/login.php
@@ -0,0 +1,45 @@
+<?php
+$_db_host = "localhost";
+$_db_database = "web";
+$_db_username = "web";
+$_db_password = "web";
+
+session_start();
+
+$conn = new mysqli($_db_host, $_db_username, $_db_password, $_db_database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ((isset($_POST["submit"])) && !empty($_POST["submit"])) {
+    $_username = $conn->real_escape_string($_POST["username"]);
+    $_password = $conn->real_escape_string($_POST["password"]);
+
+    $_password = "saver" . $_password;
+    $_sql = "SELECT * FROM login_username WHERE username='$_username' AND password=md5('$_password') AND user_deleted=0 LIMIT 1";
+
+    if ($_res = $conn->query($_sql)) {
+        if($_res->num_rows > 0){
+            $_SESSION["login"] = 1;
+            $_SESSION["user"] = $_res->fetch_assoc();
+
+            $_sql = "UPDATE login_username SET last_login=NOW() WHERE id=" . $_SESSION["user"]["id"];
+            $conn->query($_sql);
+        }
+    } else {
+        include("login.html");
+        exit;
+    }
+} else {
+    include("login.html");
+}
+
+$conn->close();
+
+if ($_SESSION["login"] != 1) {
+    include("login.html");
+    exit;
+}
+
+echo "<br>User " . $_SESSION["user"]["username"] . " is logged in since " . $_SESSION["user"]["last_login"] . "<br>";
