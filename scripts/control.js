4ca1799046c121d11a554c29546d52b865da6df4
FitzingerJ <jacki.fitzinger@gmail.com>
2020-05-18 13:49:59 +0200

    ControlPanel

---
+++ scripts/control.js
@@ -0,0 +1,16 @@
+let lightOn = 0;
+let windowOpen = 0;
+let heaterOn = 0;
+let speakerOn = 0;
+function lightControl(){
+
+}
+function windowControl(){
+
+}
+function heaterControl(){
+
+}
+function speakerControl(){
+
+}
--- scripts/newRoom.js
+++ scripts/newRoom.js
@@ -51,7 +51,7 @@ function createRoom(){
       var add = document.getElementById("addRoom");
       var parent = add.parentNode;
       var helper = document.createElement('div');
-      helper.innerHTML = "<a id='" + roomCount + "'><div id='" + roomCount + "' class='room'><h3 onclick='removeRoom(" + roomCount + ")' id='removeButton'>x</h3><p>" + roomName + "</p></div></a>";
+      helper.innerHTML = "<a id='" + roomCount + "'><div id='" + roomCount + "' class='room'><h3 onclick='removeRoom(" + roomCount + ")' id='removeButton'>x</h3><p>" + roomName + "</p><div id='imgContainer'><img onclick='lightControl(" + roomCount + ")' id='lightIcon" + roomCount + "' src='images/LightIconOff.png'><img onclick='windowControl(" + roomCount + ")' id='windowIcon" + roomCount + "' src='images/WindowIconOpen.png'><img onclick='heaterControl(" + roomCount + ")' id='heaterIcon" + roomCount + "' src='images/HeaterIconOff.png'><img onclick='speakerControl(" + roomCount + ")' id='speakerIcon" + roomCount + "' src='images/SpeakerIconOff.png'></div></div></a>";
       localStorage.setItem("roomCount", roomCount);
       localStorage.setItem(""+roomCount, roomName);
       roomCount++;
@@ -81,6 +81,44 @@ function hideBox(){
   }
 }
 
+
+function lightControl(id){
+  let lightArray = document.getElementById(`lightIcon${id}`).src.split("/");
+  let light = lightArray[lightArray.length - 1];
+  if(light == 'LightIconOff.png'){
+    document.getElementById(`lightIcon${id}`).src = 'images/LightIconOn.png';
+  } else {
+    document.getElementById(`lightIcon${id}`).src = 'images/LightIconOff.png';
+  }
+}
+function windowControl(id){
+  let windowArray = document.getElementById(`windowIcon${id}`).src.split("/");
+  let windowImg = windowArray[windowArray.length - 1];
+  if(windowImg == 'WindowIconOpen.png'){
+    document.getElementById(`windowIcon${id}`).src = 'images/WindowIconClosed.png';
+  } else {
+    document.getElementById(`windowIcon${id}`).src = 'images/WindowIconOpen.png';
+  }
+}
+function heaterControl(id){
+  let heaterArray = document.getElementById(`heaterIcon${id}`).src.split("/");
+  let heater = heaterArray[heaterArray.length - 1];
+  if(heater == 'HeaterIconOff.png'){
+    document.getElementById(`heaterIcon${id}`).src = 'images/HeaterIconOn.png';
+  } else {
+    document.getElementById(`heaterIcon${id}`).src = 'images/HeaterIconOff.png';
+  }
+}
+function speakerControl(id){
+  let speakerArray = document.getElementById(`speakerIcon${id}`).src.split("/");
+  let speaker = speakerArray[speakerArray.length - 1];
+  if(speaker == "SpeakerIconOff.png"){
+    document.getElementById(`speakerIcon${id}`).src = 'images/SpeakerIconOn.png';
+  } else {
+    document.getElementById(`speakerIcon${id}`).src = 'images/SpeakerIconOff.png';
+  }
+}
+
 //Load Your Rooms
 function loadRooms(roomName){
   if(roomName != ""){
@@ -88,7 +126,7 @@ function loadRooms(roomName){
       var add = document.getElementById("addRoom");
       var parent = add.parentNode;
       var helper = document.createElement('div');
-      helper.innerHTML = "<a id='" + roomCount + "'><div id='" + roomCount + "' class='room'><h3 onclick='removeRoom(" + roomCount + ")' id='removeButton'>x</h3><p>" + roomName + "</p></div></a>";
+      helper.innerHTML = "<a id='" + roomCount + "'><div id='" + roomCount + "' class='room'><h3 onclick='removeRoom(" + roomCount + ")' id='removeButton'>x</h3><p>" + roomName + "</p><div id='imgContainer'><img onclick='lightControl(" + roomCount + ")' id='lightIcon" + roomCount + "' src='images/LightIconOff.png'><img onclick='windowControl(" + roomCount + ")' id='windowIcon" + roomCount + "' src='images/WindowIconOpen.png'><img onclick='heaterControl(" + roomCount + ")' id='heaterIcon" + roomCount + "' src='images/HeaterIconOff.png'><img onclick='speakerControl(" + roomCount + ")' id='speakerIcon" + roomCount + "' src='images/SpeakerIconOff.png'></div></div></a>";
       roomCount++;
       while (helper.firstChild) {
       parent.insertBefore(helper.firstChild, add);
